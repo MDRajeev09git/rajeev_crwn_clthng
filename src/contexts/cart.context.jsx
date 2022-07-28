3337f11244b7fc195368f24c3fff9b725de54428
MDRajeev09git <Rajeevkd710@gmail.com>
2022-07-28 18:29:10 +0530

    fifth commit till redux

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,11 +1,11 @@
-import {createContext,useEffect,useState} from 'react';
-
+import {createContext,useState,useReducer} from 'react';
+import {createAction} from '../utils/reducer/reducer.utils';
 
 const addCartItem =(cartItems,productToAdd) =>{
 const existingCartItem =cartItems.find(
   (cartItem) => cartItem.id === productToAdd.id);
 
-
+  
 if(existingCartItem){
   return cartItems.map((cartItem) =>
    cartItem.id === productToAdd.id
@@ -13,7 +13,6 @@ if(existingCartItem){
   : cartItem
   );
 }
-
 return [...cartItems,{...productToAdd,quantity:1}];
 };
 
@@ -23,7 +22,8 @@ const removeCartItem = (cartItems,cartItemToRemove)=>{
     );
     
     if(existingCartItem.quantity === 1){
-      return cartItems.filter((cartItem) =>cartItem.id !== cartItemToRemove.id);
+      return cartItems.filter((cartItem) =>
+      cartItem.id !== cartItemToRemove.id);
     }
 
     return cartItems.map((cartItem) =>
@@ -33,51 +33,96 @@ const removeCartItem = (cartItems,cartItemToRemove)=>{
    );
  };
     
-    const clearCartItem =(cartItems,cartItemToClear) =>
-     cartItems.filter((cartItem) =>cartItem.id !== cartItemToClear.id);
+  
+const CART_ACTION_TYPES={
+  SET_IS_CART_OPEN: 'SET_IS_CART_OPEN',
+  SET_CART_ITEMS: 'SET_CART_ITEMS',
+  SET_CART_COUNT: 'SET_CART_COUNT',
+  SET_CART_OPEN :'SET_CART_OPEN'
+}
+const INITIAL_STATE={
+  isCartOpen:false,
+  cartItems:[],
+  cartCount:0,
+  cartTotal:0
+};
 
-    
+const  cartReducer =(state,action) =>{
+  const {type,payload} =action;
+ 
+  switch(type){
+    case CART_ACTION_TYPES.SET_CART_ITEMS:
+      return{
+        ...state,
+        ...payload,
+      };
+      case CART_ACTION_TYPES.SET_CART_OPEN:
+        return{
+          ...state,
+          isCartOpen:payload,
+        };
+    default:
+      throw new Error (`unhandled type of ${type}in cartReducer`)
+
+  }
+
+};
+
+const clearCartItem =(cartItems,cartItemToClear) =>
+cartItems.filter((cartItem) =>cartItem.id !== cartItemToClear.id);
+
+
 export const CartContext = createContext({
-    isCartOpen:false,
-    setIsCartOpen :() => {},
-    cartItems:[],
-    addItemToCart:() => {},
-    removeItemToCart:() => {},
-    clearItemFromCart:()=> {},
-    cartCount:0,
-    cartTotal:0
+isCartOpen:false,
+setIsCartOpen :() => {},
+cartItems:[],
+addItemToCart:() => {},
+removeItemToCart:() => {},
+clearItemFromCart:()=> {},
+cartCount:0,
+cartTotal:0
 });
 
 export const CartProvider =({children}) => {
-    const [isCartOpen,setIsCartOpen] = useState(false);
-    const [cartItems,setCartItems] =useState([]);
-    const [cartCount,setCartCount]= useState(0);
-    const [cartTotal,setCartTotal]= useState(0);
-
-    useEffect (() => {
-      const newCartCount = cartItems.reduce(
+  const [isCartOpen, setIsCartOpen] = useState(false);
+         const [{cartItems,cartCount,cartTotal},dispatch] =
+           useReducer(cartReducer,INITIAL_STATE);
+ 
+     const updateCartItemsReducer =(newCartItems)=> {
+      const newCartCount = newCartItems.reduce(
         (total,cartItem) => total+cartItem.quantity, 0);
-         setCartCount(newCartCount);
-    },[cartItems]);
-
-    useEffect (() => {
-      const newCartTotal = cartItems.reduce(
-        (total,cartItem) => total+cartItem.quantity * cartItem.price, 0);
-         setCartTotal(newCartTotal);
-    },[cartItems]);
-
-      const addItemToCart =(productToAdd) => {
-           setCartItems(addCartItem(cartItems,productToAdd));
+        
+        const newCartTotal = newCartItems.reduce(
+          (total,cartItem) => total+cartItem.quantity * cartItem.price, 0);
+         
+          const payload = {
+            cartItems,
+            cartCount: newCartCount,
+            cartTotal: newCartTotal,
+          };
+            
+            
+          dispatch(
+            createAction(CART_ACTION_TYPES.SET_CART_ITEMS,{cartItems:newCartItems,
+              cartTotal:newCartTotal, cartCount : newCartCount})
+            
+           );
      };
 
+      const addItemToCart =(productToAdd) => {
+       const newCartItems=addCartItem(cartItems,productToAdd);
+       updateCartItemsReducer(newCartItems);
+     };
       const removeItemToCart =(cartItemToRemove) => {
-        setCartItems(removeCartItem(cartItems,cartItemToRemove));
+      const newCartItems=removeCartItem(cartItems,cartItemToRemove);
+       updateCartItemsReducer(newCartItems);
 };
-       
-   const clearItemFromCart =(cartItemToClear) => {
-    setCartItems(clearCartItem(cartItems,cartItemToClear));
+       const clearItemFromCart =(cartItemToClear) => {
+       const newCartItems=clearCartItem(cartItems,cartItemToClear);
+        updateCartItemsReducer(newCartItems);
 };
-
+      //  const  setIsCartOpen = (bool) =>{
+      //   dispatch(createAction( CART_ACTION_TYPES.SET_IS_CART_OPEN,bool));       }
     const value = { isCartOpen,setIsCartOpen,addItemToCart,clearItemFromCart,removeItemToCart,cartItems,cartCount,cartTotal};
 
     return<CartContext.Provider value={value}>{children}</CartContext.Provider>;
